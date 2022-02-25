d80c35fda5cfda6f6fb95a490c61971bc5966163
Your Name <you@example.com>
2022-02-25 13:31:02 +0800

    event page added

---
+++ pages/event.tsx
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+
+export default class event extends Component {
+    render(): React.ReactNode {
+        return <div className = " bg-slate-600 text-white py-2"></div>
+        
+    }
+}
