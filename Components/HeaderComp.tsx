ac1c59c6cacd9395ead1264324261a37a3d7b7c7
Your Name <you@example.com>
2022-02-28 16:15:49 +0800

    Header Component

---
+++ Components/HeaderComp.tsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class HeaderComp extends Component {
+    
+        render(): React.ReactNode {
+            return <div className = " bg-slate-600 text-white py-2">
+                <p className = "text-center">This is the Header Component</p>
+            </div>
+            
+        }
+    }
---
+++ Components/Layout.tsx
@@ -0,0 +1,6 @@
+const layout = ({props}: any) => {
+    return ( <div className = " bg-slate-600 text-white py-2">
+        {props}
+    </div>);
+}
+export default layout;
